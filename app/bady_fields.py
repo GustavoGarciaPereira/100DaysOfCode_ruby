70fd04f91904b967d8b22bad6868b4fb1624488c
gustavo <gusgurtavo@gmail.com>
2022-02-18 23:08:52 -0300

    dia 12 body-fields

---
+++ app/bady_fields.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = Field(
+        None,
+        title="title aqui", max_length=200
+    )
+    price: float = Field(..., gt=0, description="price description")
+    tax: Optional[float] = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item = Body(..., embed=True)):
+    results = {"item_id": item_id, "item": item}
+    return results
